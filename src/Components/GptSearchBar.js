025455b4bd4228a0f2d9d1db1d4c6c11ed6851ad
Shani <shanidevkashyap8913@gmail.com>
2025-01-20 22:14:44 +0530

    Use TMDB-Search API to get movies whose theme I search in my search-box provided in GptSearch

--- src/Components/GptSearchBar.js
+++ src/Components/GptSearchBar.js
@@ -3,23 +3,61 @@ import Lang from "../Utils/Lang";
 import { useSelector } from "react-redux";
 import { useRef } from "react";
 import genAI from "../Utils/GeminiAi";
-
+import { API_OPTIONS } from "../Utils/constant";
 
 const GptSearchBar = () => {
   const searchText = useRef(null);
+  {
+    /*Search Function where I will search movie in TMDB*/
+  }
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`,
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json?.results;
+  };
   const handleGPTSearchClick = async () => {
     console.log(searchText.current.value);
     // make an API call to Gemini API to get the movie results
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const gptQuery =
-  "Act as a Movie Recommendation system and suggest some movies for the query : " +
-  searchText.current.value +
-  ". only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
 
-  const gptResult=await model.generateContent(gptQuery);
-  console.log(gptResult?.response?.candidates[0]?.content?.parts[0]?.text);
+    const gptResult = await model.generateContent(gptQuery);
+    
+    {/* With the help of split(",") we are able to get result data in the form of string of array */}
+
+    const gptMovies = gptResult?.response?.candidates[0]?.content?.parts[0]?.text.split(",");
+    console.log(gptMovies);
+
+    {
+      /*For each movie now I will search in TMDB API*/
+
+      /* Here , searchMovieTMDB will be called 5 times and it will return promise not results , because async function returns promises immmediately and it will take time to return results
+      Hence , it will return array of promises in the form : [promise , promise , promise , promise , promise] */
+
+
+      /* Time, Tide and JavaScript waits for none */
+    }
+
+    const promiseArray = gptMovies.map((movie) =>searchMovieTMDB(movie));
+
+    /*Promise.all() ==> this function basically takes array of promises and it is used to resolve all promises*/ 
+
+    // Here I wrote await because now it will wait until all promises are resolved
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+    console.log(tmdbResults);
+
 
   };
+
+  
 
   const preferredLanguage = useSelector((store) => store?.config?.lang);
   console.log(preferredLanguage);
