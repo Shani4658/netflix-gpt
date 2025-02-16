a73f686a827ba7f77f50611dd32c28d81fb8e04c
Shani <shanidevkashyap8913@gmail.com>
2025-02-16 10:26:13 +0530

    Add background music

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -11,7 +11,7 @@ const VideoBackground = ({ movieId }) => {
     <div>
       <iframe
         className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?&autoplay=1&mute=1"}
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?&autoplay=1&mute=0"}
         title="YouTube video player"
         frameBorder = "0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
